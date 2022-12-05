4b6df9d044e7c68e4f612d9dd1a71720c894246c
smoothwarrior <wildbillcody90@gmail.com>
2022-12-04 19:36:53 -0600

    new file:   .gitignore new file:   Cargo.lock new file:   Cargo.toml new file:   src/lib.rs new file:   src/main.rs
    src/lib.rs contains all structs and functions related to the CPU struct itself.
    src/main.rs is where all cpu instructions should be called

---
+++ src/lib.rs
@@ -0,0 +1,175 @@
+
+#[derive(Debug)]
+pub struct Mem {                  // this struct is used to store memory data in the form of ram
+	pub memory: [u8; 256]
+}
+impl Mem {
+	pub fn new() -> Mem {
+		Mem {
+			memory: [0; 256]
+		}
+	}
+}
+
+#[derive(Debug)]
+pub struct Register {               // this struct is used to create a register for use in struct RegFile
+	pub name: String,
+	pub data: u8,
+}
+impl Register {
+	pub fn new(name: String) -> Register {
+		Register {
+			name: name,
+			data: 0,
+		}
+	}
+}
+
+#[derive(Debug)]
+pub struct RegFile {                    // this struct creates an array of registers that the CPU can use
+	pub registers: [Register; 5],
+}
+impl RegFile {
+	pub fn new(a: Register, b: Register, c: Register, d: Register, bp: Register) -> RegFile {
+		let regis = [a, b, c, d, bp];
+		RegFile {
+			registers: regis
+		}
+	}
+}
+
+#[derive(Debug)]
+pub struct CPU {                        // this is the main struct of CPU. it has access to the other mentioned structs
+	pub registers: RegFile,             // the CPU's new() function will automatically create necessary memory and register structs
+	pub memory: Mem,
+}
+impl CPU {
+	pub fn new() -> CPU {
+		let a = Register::new("A".to_string());
+		let b = Register::new("B".to_string());
+		let c = Register::new("C".to_string());
+		let d = Register::new("D".to_string());
+		let bp = Register::new("BP".to_string());
+		CPU {
+			registers: RegFile::new(a, b, c, d, bp),
+			memory: Mem::new(),
+		}
+	}
+	// replace data in specified register with new data
+	pub fn update_reg_data(&mut self, reg: usize, data: u8) {
+		self.registers.registers[reg].data = data;
+		println!("Pushing immediate value of {:?} to {:?}", data, self.registers.registers[reg].name);
+	}
+	// add two registers together and stores the result in the first register
+	pub fn add_reg_reg(&mut self, reg1: usize, reg2: usize) {
+		let val1 = self.registers.registers[reg1].data;
+		let val2 = self.registers.registers[reg2].data;
+		let result = val1 + val2;
+		self.registers.registers[0].data = result;
+		println!("{:?} added to {:?}", self.registers.registers[reg1].name, self.registers.registers[reg2].name);
+		println!("{:?} now holds {:?}", self.registers.registers[0].name, result);
+	}
+	// subtracts two registers storing the result in the first register
+	pub fn sub_reg_reg(&mut self, reg1: usize, reg2: usize) {
+		let val1 = self.registers.registers[reg1].data;
+		let val2 = self.registers.registers[reg2].data;
+		let result = val1 - val2;
+		self.registers.registers[0].data = result;
+		println!("{:?} subtracted from {:?}", self.registers.registers[reg1].name, self.registers.registers[reg2].name);
+		println!("{:?} now holds {:?}", self.registers.registers[0].name, result);
+	}
+	// or's two registers storing the result in the first register
+	pub fn or_reg_reg(&mut self, reg1: usize, reg2: usize) {
+		let val1 = self.registers.registers[reg1].data;
+		let val2 = self.registers.registers[reg2].data;
+		let result = val1 | val2;
+		self.registers.registers[0].data = result;
+		println!("{:?} ORed with {:?}", self.registers.registers[reg1].name, self.registers.registers[reg2].name);
+		println!("{:?} now holds {:?}", self.registers.registers[0].name, result);
+	}
+	// and's two registers storing the result in the first register
+	pub fn and_reg_reg(&mut self, reg1: usize, reg2: usize) {
+		let val1 = self.registers.registers[reg1].data;
+		let val2 = self.registers.registers[reg2].data;
+		let result = val1 & val2;
+		self.registers.registers[0].data = result;
+		println!("{:?} ANDed with {:?}", self.registers.registers[reg1].name, self.registers.registers[reg2].name);
+		println!("{:?} now holds {:?}", self.registers.registers[0].name, result);
+	}
+	// xor's two registers storing the result in the first register
+	pub fn xor_reg_reg(&mut self, reg1: usize, reg2: usize) {
+		let val1 = self.registers.registers[reg1].data;
+		let val2 = self.registers.registers[reg2].data;
+		let result = val1 ^ val2;
+		self.registers.registers[0].data = result;
+		println!("{:?} XORed with {:?}", self.registers.registers[reg1].name, self.registers.registers[reg2].name);
+		println!("{:?} now holds {:?}", self.registers.registers[0].name, result);
+	}
+	// not's a register storing the result in the first register
+	pub fn not_reg(&mut self, reg: usize) {
+		let val = self.registers.registers[reg].data;
+		let result = !val;
+		self.registers.registers[0].data = result;
+		println!("{:?} NOTed", self.registers.registers[reg].name);
+		println!("{:?} now holds {:?}", self.registers.registers[0].name, result);
+	}
+	// shift the data bits of a register to the left
+	pub fn shl_reg(&mut self, reg: usize) {
+		let val = self.registers.registers[reg].data;
+		let result = val << 1;
+		self.registers.registers[reg].data = result;
+		println!("{:?} shifted left", self.registers.registers[reg].name);
+		println!("{:?} now holds {:?}", self.registers.registers[reg].name, result);
+	}
+	// shift the data bits of a register to the right
+	pub fn shr_reg(&mut self, reg: usize) {
+		let val = self.registers.registers[reg].data;
+		let result = val >> 1;
+		self.registers.registers[reg].data = result;
+		println!("{:?} shifted right", self.registers.registers[reg].name);
+		println!("{:?} now holds {:?}", self.registers.registers[reg].name, result);
+	}
+	// moves data from one register to another
+	pub fn mov_reg_reg(&mut self, source: usize, dest: usize) {
+		let data = self.registers.registers[source].data;
+		self.registers.registers[dest].data = data;
+		println!("Moved {:?} to {:?}", self.registers.registers[source].name, self.registers.registers[dest].name);
+		println!("{:?} now holds {:?}", self.registers.registers[dest].name, data);
+	}
+	// stores the data of a register to memory located at "BP" registers data value
+	pub fn store_reg(&mut self, source: usize) {
+		let data = self.registers.registers[source].data;
+		let dest = self.registers.registers[4].data as usize;
+		self.memory.memory[dest] = data;
+		println!("{:?} stored to memory at {:?}", self.registers.registers[source].name, dest);
+	}
+	// loads data from memory with address located in "BP" register into specified register
+	pub fn load_data(&mut self, dest: usize) {
+		let addr: usize = self.registers.registers[4].data.into();
+		self.registers.registers[dest].data = self.memory.memory[addr];
+		println!("{:?} is loading data from memory at {:?}", self.registers.registers[dest].name, addr);
+		println!("{:?} now holds {:?}", self.registers.registers[dest].name, self.registers.registers[dest].data);
+	}
+	// increases the value of a register by 1
+	pub fn inc_reg(&mut self, reg: usize) {
+		if self.registers.registers[reg].data < 255 {
+			self.registers.registers[reg].data += 1;
+			let data = self.registers.registers[reg].data;
+			println!("{:?} incremented to {:?}", self.registers.registers[reg].name, data);
+		} else {
+			println!("{:?} overflowed! setting to 0", self.registers.registers[reg].name);
+			self.registers.registers[reg].data = 0;
+		}
+	}
+	// resets all data in CPU to 0
+	pub fn reset_all_data(&mut self) {
+		println!("Resetting all data");
+		for i in 0..5 {
+			self.registers.registers[i].data = 0;
+		}
+		for i in 0..self.memory.memory.len() {
+			self.memory.memory[i] = 0;
+		}
+		println!("All data reset");
+	}
+}
---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use my_project::CPU;
+
+fn print_cpu_stats(cpu: &CPU) {
+	println!("\n");
+	println!("reg: {:?} data: {:?}",cpu.registers.registers[0].name, cpu.registers.registers[0].data);
+	println!("reg: {:?} data: {:?}",cpu.registers.registers[1].name, cpu.registers.registers[1].data);
+	println!("reg: {:?} data: {:?}",cpu.registers.registers[2].name, cpu.registers.registers[2].data);
+	println!("reg: {:?} data: {:?}",cpu.registers.registers[3].name, cpu.registers.registers[3].data);
+	println!("reg: {:?} data: {:?}",cpu.registers.registers[4].name, cpu.registers.registers[4].data);
+}
+
+fn print_memory_data(cpu: &CPU) {
+	println!("Mem data: \n{:?}", cpu.memory.memory);
+}
+
+fn psuedo_rng(cpu: &mut CPU, regs: [usize; 5]) {
+	cpu.store_reg(regs[0]);
+	cpu.shr_reg(regs[0]);
+	cpu.mov_reg_reg(regs[0], regs[1]);
+	cpu.load_data(regs[0]);
+	cpu.inc_reg(regs[4]);
+	cpu.xor_reg_reg(regs[0], regs[1]);
+	cpu.store_reg(regs[0]);
+	cpu.shl_reg(regs[0]);
+	cpu.mov_reg_reg(regs[0], regs[1]);
+	cpu.load_data(regs[0]);
+	cpu.inc_reg(regs[4]);
+	cpu.xor_reg_reg(regs[0], regs[1]);
+	cpu.store_reg(regs[0]);
+	cpu.shr_reg(regs[0]);
+	cpu.shr_reg(regs[0]);
+	cpu.mov_reg_reg(regs[0], regs[1]);
+	cpu.load_data(regs[0]);
+	cpu.inc_reg(regs[4]);
+	cpu.xor_reg_reg(regs[0], regs[1]);
+	cpu.store_reg(regs[0]);
+}
+
+fn main() {
+	println!("Compiled seccessfully");
+	println!("Running...\n");
+	let mut cpu = CPU::new(); // Creates the CPU
+	let a: usize = 0;              // Binding for register ID
+	let b: usize = 1;              // Binding for register ID
+	let c: usize = 2;              // Binding for register ID
+	let d: usize = 3;              // Binding for register ID
+	let bp: usize = 4;             // Binding for register ID
+	let regs = [a, b, c, d, bp]; // this is used for passing register ID's to functions
+	println!("Initializing CPU CORE");
+	cpu.update_reg_data(a, 0); // this command will replace the registers current data with new data provided
+	cpu.update_reg_data(b, 0);
+	cpu.update_reg_data(c, 0);
+	cpu.update_reg_data(d, 0);
+	cpu.update_reg_data(bp, 0);
+	println!("Loading memory");
+	println!("Memory ready\nMemory size in bytes: 256\n");
+	println!("CPU Running...\n");
+	cpu.update_reg_data(a, 5);
+	cpu.inc_reg(a);                         // this command will increment the registers data by 1
+	cpu.store_reg(a);               // this command will store the registers data into memory
+	cpu.inc_reg(bp);
+	cpu.update_reg_data(b, 200);
+	cpu.store_reg(b);
+	cpu.load_data(c);                 // this command will load the data from memory into the register
+	cpu.add_reg_reg(a, c);
+	cpu.inc_reg(bp);
+	cpu.store_reg(a);
+	cpu.reset_all_data();                   // this command will reset all registers and memory data to 0
+	println!("\n");
+	cpu.update_reg_data(a, 29);
+	for _i in 0..=10 {
+		psuedo_rng(&mut cpu, regs);          // example of a function with a mutable reference to the CPU and an array of register ID's
+	}
+	print_cpu_stats(&cpu);                   // this function will print current status of the CPU's registers
+	print_memory_data(&cpu);                 // this function will print current status of the CPU's memory
+
+
+
+	println!("\nProgram end\nShutting down virtualized CPU...\nGoodbye");
+}
